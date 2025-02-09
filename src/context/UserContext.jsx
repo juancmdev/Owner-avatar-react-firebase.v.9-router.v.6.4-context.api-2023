5d3b980af9952edf8ada63504e97b72ce4752dee
juancmdev <juancmorenoj@gmail.com>
2025-02-09 15:47:24 -0500

    Update UserContext.jsx

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
+import { auth } from "../config/firebase";
 
 const UserContext = createContext();
 
 export default function UserContextProvider({ children }) {
-    const [user, setUser] = useState(false);
+  const [user, setUser] = useState(false);
 
-    return (
-        <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
-    );
+  useEffect(() => {
+    const unsuscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return unsuscribe;
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
 }
 
 export const useUserContext = () => useContext(UserContext);
