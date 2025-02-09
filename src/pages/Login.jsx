e6fa42d917aa86136198606fb07f9204d6569aba
juancmdev <juancmorenoj@gmail.com>
2025-02-09 12:29:55 -0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="Ingrese email" />
-        <input type="password" placeholder="Ingrese contraseña" />
+        <input
+          type="text"
+          placeholder="Ingrese email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Ingrese contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
     </>
