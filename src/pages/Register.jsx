678098a52cdc5caec53cfa26f261284ea2f9ef28
juancmdev <juancmorenoj@gmail.com>
2025-02-09 13:05:56 -0500

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { register } from "../config/firebase";
+
 const Register = () => {
-    return "Register";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const credentialUser = await register({
+        email,
+        password,
+      });
+      console.log(credentialUser);
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ingrese email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Ingrese contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
